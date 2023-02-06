c346cefd6f2353f259cf09754f3008ee962a147e
Leah <leah.jenkins2019@gmail.com>
2023-02-06 17:28:47 -0500

    tests should pass

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function mondayWork(task = "go to the office"){
+    console.log(`This Monday, I will ${task} .`)
+}
+
+function saturdayFun(event = "roller-skate"){
+    console.log(`This Saturday, I will ${event}`)
+}
+
+function wrapAdjective(symbol = '*'){
+const  encouragingPromptFunction = function (thing1 = "special"){
+    return `You are ${symbol} ${thing1}`
+}
+return encouragingPromptFunction}
